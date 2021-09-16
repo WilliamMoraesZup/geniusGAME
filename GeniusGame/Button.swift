72d64adfe1ffc542a967bd3407e8520b99279ad4
WilliamMoraesZup <william.moraes@zup.com.br>
2021-09-16 15:44:28 -0300

    pulse button created

---
+++ GeniusGame/Button.swift
@@ -0,0 +1,8 @@
+//
+//  Botao.swift
+//  GeniusGame
+//
+//  Created by William Moraes da Silva on 16/09/21.
+//
+
+import Foundation
---
+++ GeniusGame/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  GeniusGame
+//
+//  Created by William Moraes da Silva on 16/09/21.
+//
+
+import Foundation
